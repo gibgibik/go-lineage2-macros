826431fabdd28eddbda4c63e90d3d44ca9313730
Gib <yd6rth4kdr@gmail.com>
2025-07-10 10:23:09 +0300

    implementation

--- cmd/webserver.go
+++ cmd/webserver.go
@@ -345,7 +345,7 @@ func startHandler(ctx context.Context, cnf *core.Config) func(w http.ResponseWri
 							break
 						}
 						runAction := runStack[i]
-						logger.Debug("run action: " + runAction.action)
+						logger.Debug("run action: "+runAction.action, " ", i)
 						if !checkUseCondition(runAction.startTargetCondition) {
 							i += 1
 							continue
