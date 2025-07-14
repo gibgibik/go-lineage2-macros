dc2c35fc23d53f08581a607908478cbe223983be
Gib <yd6rth4kdr@gmail.com>
2025-07-14 12:26:25 +0300

    remove fmt

--- internal/service/server.go
+++ internal/service/server.go
@@ -53,17 +53,29 @@ func FindBounds(url string, logger *zap.SugaredLogger) ([][]int, error) {
 	if err != nil {
 		return nil, err
 	}
+	var boxes BoundsResult
 	var result BoundsResult
-	err = json.Unmarshal(bounds, &result)
+	err = json.Unmarshal(bounds, &boxes)
 	if err != nil {
 		logger.Error("parse bounds json error: ", err.Error())
 		return nil, nil
 	}
-	sort.Slice(result.Boxes, func(i, j int) bool {
-		return result.Boxes[i][1] > result.Boxes[j][1]
+	sort.Slice(boxes.Boxes, func(i, j int) bool {
+		return boxes.Boxes[i][1] < boxes.Boxes[j][1]
 	})
-	//if len(result.Boxes) > 10 {
-	//	result.Boxes = result.Boxes[:10]
+	sort.Slice(boxes.Boxes, func(i, j int) bool {
+		return boxes.Boxes[i][0] < boxes.Boxes[j][2]
+	})
+	for i := 0; i < len(boxes.Boxes); i++ {
+		if i+1 < len(boxes.Boxes) && (boxes.Boxes[i+1][0]-boxes.Boxes[i][2]) <= 3 { //glue nested boxes
+			result.Boxes[i+1] = []int{boxes.Boxes[i][0], boxes.Boxes[i][1], boxes.Boxes[i+1][2], boxes.Boxes[i+1][3]}
+			i++
+		} else {
+			result.Boxes = append(result.Boxes, result.Boxes[i])
+		}
+	}
+	//if len(boxes.Boxes) > 10 {
+	//	boxes.Boxes = boxes.Boxes[:10]
 	//}
 	return result.Boxes, nil
 }
