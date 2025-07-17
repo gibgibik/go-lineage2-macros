5b422d255fdf56b2cdec1852e06b58ef9dfbc085
Gib <yd6rth4kdr@gmail.com>
2025-07-17 11:12:05 +0300

    improvement

--- internal/core/http/http_client.go
+++ internal/core/http/http_client.go
@@ -83,7 +83,7 @@ func (cl *HttpClient) RawRequest(path string, method string, body io.Reader) (re
 	return nil, fmt.Errorf("failed after %d retries: %v", maxRetries, err)
 }
 
-func IniHttpClient(baseUrl string) *HttpClient {
+func IniHttpClient(baseUrl string) {
 	HttpCl = &HttpClient{
 		baseUrl: baseUrl,
 		Client: &http.Client{
