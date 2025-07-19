8da709e7e3a7e46db0d65376d97c24c6053ec985
Gib <yd6rth4kdr@gmail.com>
2025-07-19 23:32:36 +0300

    1

--- cmd/webserver.go
+++ cmd/webserver.go
@@ -626,7 +626,7 @@ func switchWindow(pid uint32, controlCl *service.Control, logger *zap.SugaredLog
 	if controlCl != nil {
 		controlCl.Cl.SendKey(ch9329.ModLeftAlt, "")
 		time.Sleep(time.Millisecond * 50)
-		controlCl.Cl.SendKey(0, "tab")
+		controlCl.Cl.SendKey(ch9329.ModLeftAlt, "tab")
 		time.Sleep(time.Millisecond * 50)
 		controlCl.Cl.SendKey(ch9329.ModLeftAlt, "")
 		time.Sleep(time.Millisecond * 50)
