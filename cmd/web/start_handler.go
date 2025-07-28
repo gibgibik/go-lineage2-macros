5daa0754da778288532e43c6a3427941dca54af1
Your Name <you@example.com>
2025-07-28 13:39:41 +0300

    123

--- cmd/web/start_handler.go
+++ cmd/web/start_handler.go
@@ -315,11 +315,12 @@ func startHandler(ctx context.Context, cnf *core.Config) func(w http.ResponseWri
 							//logger.Info(message)
 							i++
 							time.Sleep(time.Millisecond * time.Duration(randNum(50, 100)))
-							if windowSwitched {
-								windowSwitched = false
-								_ = switchWindow(anotherPid, controlCl, logger)
-								pidsStack[anotherPid].waitCh <- struct{}{}
-							}
+							logger.Info("tick ", i)
+						}
+						if windowSwitched {
+							windowSwitched = false
+							_ = switchWindow(anotherPid, controlCl, logger)
+							pidsStack[anotherPid].waitCh <- struct{}{}
 						}
 						//logger.Info("end interation")
 						//run stack
