ff7c1ed85c366c8186d05de9d54a302c67cd9e2a
Your Name <you@example.com>
2025-07-30 23:52:20 +0300

    1

--- cmd/web/start_handler.go
+++ cmd/web/start_handler.go
@@ -111,6 +111,7 @@ func startHandler(ctx context.Context, cnf *core.Config) func(w http.ResponseWri
 							runAction := &profilePreset.item.Preset.Items[i]
 							if runAction.Action == "" {
 								i++
+								time.Sleep(time.Millisecond * 10)
 								continue
 							}
 							if runAction.Action == service.ActionStop {
@@ -145,10 +146,12 @@ func startHandler(ctx context.Context, cnf *core.Config) func(w http.ResponseWri
 									}
 								}
 								i++
+								time.Sleep(time.Millisecond * 10)
 								continue
 							}
 							if runAction.PeriodMilliseconds > 0 && runAction.LastRun.UnixMilli() > (time.Now().UnixMilli()-runAction.PeriodMilliseconds) {
 								i++
+								time.Sleep(time.Millisecond * 10)
 								continue
 							}
 							service.PlayerStatsMutex.Lock()
@@ -158,6 +161,7 @@ func startHandler(ctx context.Context, cnf *core.Config) func(w http.ResponseWri
 								if err != nil {
 									logger.Error("check condition error: " + err.Error())
 								}
+								time.Sleep(time.Millisecond * 10)
 								continue
 							} else {
 								service.PlayerStatsMutex.Unlock()
@@ -170,6 +174,7 @@ func startHandler(ctx context.Context, cnf *core.Config) func(w http.ResponseWri
 									if err != nil {
 										logger.Error("find bounds error: " + err.Error())
 										i++
+										time.Sleep(time.Millisecond * 10)
 										continue
 									} else {
 										if controlErr == nil {
@@ -212,6 +217,7 @@ func startHandler(ctx context.Context, cnf *core.Config) func(w http.ResponseWri
 									runAction.LastRun = time.Now()
 								}
 								i++
+								time.Sleep(time.Millisecond * 10)
 								continue
 							}
 							if runAction.Action == service.ActionAssistPartyMember {
